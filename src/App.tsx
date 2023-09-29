b4731635aaa66ef8d0746760240462d6f020eb38
MTGrimm <98679259+MTGrimm@users.noreply.github.com>
2023-09-29 12:51:50 -0600

    fixed build

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Header from "./components/Header/Header";
 import About from "./components/About/About";
 import Events from "./components/Events/Events";
 import Members from "./components/Members/Members";
-import everest_bg from "./assets/images/mt_everest.jpg";
 
 function App() {
 	return (
