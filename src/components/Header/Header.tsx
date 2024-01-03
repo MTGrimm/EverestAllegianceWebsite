bf3be49e07073fec3434edc069fd2fa0ac6cb388
Aryan Timilsina <aryan.timilsina195@gmail.com>
2024-01-03 16:40:04 -0700

    go part works

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,34 @@
 import "./Header.css";
 import transparent_logo from "../../assets/images/everest_new_small.png";
+import {useRef} from 'react'
 
 const Header = () => {
+    const emailRef = useRef(null)
+
+    const handleSubmit = async() => {
+        console.log("works?");
+        if (emailRef.current != null) {
+            const email = emailRef.current['value'];
+            const formData = new FormData();
+            formData.append('email', email);
+            
+            try {
+                const response = await fetch("http://localhost:8080/newEmail", {
+                    method: "POST",
+                    body: formData,
+                });
+
+                if (response.ok) {
+                    console.log("email added");
+                } else {
+                    console.log("failed");
+                }
+            } catch (error) {
+                console.log("error occurred: ", error);
+            }
+        }
+    }
+
 	return (
 		<div id="mission" className="everest__header section__padding">
 			<div className="everest__header-content">
@@ -10,8 +37,8 @@ const Header = () => {
 				</div>
 
 				<div className="everest__header-content__input">
-					<input type="email" placeholder="Your Email Address" />
-					<button type="button">Get Updates</button>
+					<input type="email" ref={emailRef} placeholder="Your Email Address" />
+					<button onClick={handleSubmit} type="button">Get Updates</button>
 				</div>
 			</div>
 		</div>
